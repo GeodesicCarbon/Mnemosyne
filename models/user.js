a454557943ba8abfea32efa35de28c1b09a50757
Vadim Gurkalov <vadim.gurkalov@aalto.fi>
2020-01-24 23:01:53 +0200

    Added Initial User schema

---
+++ models/user.js
@@ -0,0 +1,42 @@
+// Ladataan tarvittavat moduulit
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+// Luodaan mongoose-skeema käyttäjille ja palautetaan siitä malli
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  name: String,
+  passwordHash: String,
+  notes : [ // Käyttäjän lisäämän muistiinpanoihin viitataan sen mallissa
+    {
+      type: String, //placeholder muistiinpanoskeemalle
+      required: true,
+      default: 'Test Note'
+    }
+  ],
+})
+
+// Käytetää uniqueValidator-pluginiä jotta validointi tapahtuisi backendissä
+// eikä tietokannassa
+userSchema.plugin(uniqueValidator, { message: '{VALUE} has already been taken as username. Please select another.' })
+
+// Luodaan funktio joka siivoaa objektin ja palauttaa sen JSON-muodossa
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    // never reveal password hash
+    delete returnedObject.passwordHash
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
