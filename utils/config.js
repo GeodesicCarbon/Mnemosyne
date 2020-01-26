9368fc542460a4420a66d04f18d05dfff42cb797
Vadim Gurkalov <vadim.gurkalov@aalto.fi>
2020-01-26 23:41:35 +0200

    debugging github actions

--- utils/config.js
+++ utils/config.js
@@ -9,7 +9,7 @@ let MONGODB_URI = process.env.MONGODB_URI
 if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
 }
-
+console.log(process.env.CI)
 if (process.env.CI === true) {
   MONGODB_URI = process.env.CI_MONGODB_URI
 }
