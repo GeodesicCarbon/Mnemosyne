a71cf656e46f97e065915c2fec58b68207d133ad
Vadim Gurkalov <vadim.gurkalov@aalto.fi>
2020-01-26 23:38:17 +0200

    Enabled DB URI for Github actions

--- utils/config.js
+++ utils/config.js
@@ -10,6 +10,10 @@ if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
 }
 
+if (process.env.CI === true) {
+  MONGODB_URI = process.env.CI_MONGODB_URI
+}
+
 module.exports = {
   MONGODB_URI,
   PORT
